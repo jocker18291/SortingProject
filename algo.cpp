3dcc2b1e1f2a19ab080c6e7bb605115ed842ba18
jocker18291 <dawidans714@gmail.com>
2025-03-21 20:54:44 +0100

    Add insertion sort algorithm for timsort

--- algo.cpp
+++ algo.cpp
@@ -4,7 +4,15 @@
 const int RUN = 32;
 
 void insertionSort(std::vector<int>& arr, int left, int right) {
-
+    for(int i = left + 1; i <= right; i++) {
+        int temp = arr[i];
+        int j = i - 1;
+        while(j >= left && arr[j] > temp) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = temp;
+    }
 }
 
 int main() {
